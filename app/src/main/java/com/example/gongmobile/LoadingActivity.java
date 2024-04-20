01b2fde2bc8913ca70078191f1d454dc43341cd4
matmirowski <mateusz.m@niceguys.info>
2024-04-20 23:22:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gongmobile/LoadingActivity.java
@@ -0,0 +1,34 @@
+package com.example.gongmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoadingActivity extends AppCompatActivity {
+    private static final int LOADING_SCREEN_DELAY = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loading);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, LOADING_SCREEN_DELAY);
+/*        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });*/
+    }
+}
